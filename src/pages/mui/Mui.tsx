a4d04e4a4290966c800a80ce47a60e6aa58b8565
Rick <cpchien@sinotech.org.tw>
2024-03-15 14:24:32 +0800

    建立一個新的頁面

---
+++ src/pages/mui/Mui.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Mui: React.FC = () => {
+    const navigate = useNavigate()
+
+    /**
+     * Call this function to redirect the user to the homepage.
+     */
+    const redirectToHomePage = () => {
+        navigate('/')
+    }
+
+    return (
+        <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <h1 style={{ fontSize: '4em' }}>Mui</h1>
+
+
+            <span style={{ cursor: 'pointer' }} onClick={() => redirectToHomePage()}>
+                Homepage
+            </span>
+        </div>
+    )
+}
+
+export default Mui
