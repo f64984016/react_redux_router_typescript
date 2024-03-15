e2b7c8f74b594dd9bdef07f2f085f53e80256b32
Rick <cpchien@sinotech.org.tw>
2024-03-15 14:45:13 +0800

    install  Roboto font

--- src/pages/mui/Mui.tsx
+++ src/pages/mui/Mui.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+/**
+ * Material UI uses the Roboto font by default.
+ */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const Mui: React.FC = () => {
     const navigate = useNavigate()
