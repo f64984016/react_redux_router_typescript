0199441e91cec3ebeb28139d617e2a050747b665
Rick <cpchien@sinotech.org.tw>
2024-03-19 16:57:08 +0800

    加入網格系統

--- src/pages/mui/Mui.tsx
+++ src/pages/mui/Mui.tsx
@@ -9,7 +9,9 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'
 import Toolbar from '@mui/material/Toolbar';
+
 import DrawerAppBar from '../../features/drawerAppBar/drawerAppBar';
 
 const Mui: React.FC = () => {
@@ -20,6 +22,28 @@ const Mui: React.FC = () => {
               <Toolbar />
               <h1 style={{ fontSize: '4em' }}>Try MUI</h1>
             </Box>
+            <Grid container spacing={2}>
+              <Grid xs={6} md={8}>
+                <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                  xs=6 md=8
+                </Box>
+              </Grid>
+              <Grid xs={6} md={4}>
+              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                xs=6 md=4
+                </Box>
+              </Grid>
+              <Grid xs={6} md={4}>
+              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                xs=6 md=4
+              </Box>
+              </Grid>
+              <Grid xs={6} md={8}>
+              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                xs=6 md=8
+              </Box>
+              </Grid>
+            </Grid>
         </div>
     )
 }
