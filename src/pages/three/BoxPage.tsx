5a09a569dae26b6d831aae55ec7460282a506155
Rick <rick8036@gmail.com>
2024-03-26 16:47:59 +0800

    頁面上加入 SampleBox 組件測試

--- src/pages/three/BoxPage.tsx
+++ src/pages/three/BoxPage.tsx
@@ -1,11 +1,21 @@
 import DrawerAppBar from '../../features/drawerAppBar/drawerAppBar';
+import { SampleBox } from '../../features/three/box/sampleBox';
+import { Canvas } from '@react-three/fiber'
 
 const BoxPage = (): JSX.Element => {
 
   return (
     <>
     <DrawerAppBar />
-
+    <br />
+    <br />
+    <Canvas>
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <SampleBox position={[-1.2, 0, 0]} />
+    <SampleBox position={[1.2, 0, 0]} />
+    </Canvas>
     </>
   );
 };
