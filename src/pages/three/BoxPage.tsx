8deb4fd360281c7c971830049137ae478dec833e
Rick <rick8036@gmail.com>
2024-03-26 15:57:44 +0800

    add page

---
+++ src/pages/three/BoxPage.tsx
@@ -0,0 +1,13 @@
+import DrawerAppBar from '../../features/drawerAppBar/drawerAppBar';
+
+const BoxPage = (): JSX.Element => {
+
+  return (
+    <>
+    <DrawerAppBar />
+
+    </>
+  );
+};
+
+export default BoxPage;
