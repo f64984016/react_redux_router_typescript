5488539b1be947c97bafd883df60548c39f7223c
Rick <rick8036@gmail.com>
2024-03-27 15:53:00 +0800

    增加 Item

--- src/pages/mui/Mui.tsx
+++ src/pages/mui/Mui.tsx
@@ -8,11 +8,21 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 
 import DrawerAppBar from '../../features/drawerAppBar/drawerAppBar';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Mui: React.FC = () => {
     return (
@@ -22,28 +32,22 @@ const Mui: React.FC = () => {
               <Toolbar />
               <h1 style={{ fontSize: '4em' }}>Try MUI</h1>
             </Box>
-            <Grid container spacing={2}>
-              <Grid xs={6} md={8}>
-                <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-                  xs=6 md=8
-                </Box>
+            <Box component="div" sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid xs={6} md={8}>
+                  <Item>xs=6 md=8</Item>
+                </Grid>
+                <Grid xs={6} md={4}>
+                  <Item>xs=6 md=4</Item>
+                </Grid>
+                <Grid xs={6} md={4}>
+                  <Item>xs=6 md=4</Item>
+                </Grid>
+                <Grid xs={6} md={8}>
+                  <Item>xs=6 md=8</Item>
+                </Grid>
               </Grid>
-              <Grid xs={6} md={4}>
-              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-                xs=6 md=4
-                </Box>
-              </Grid>
-              <Grid xs={6} md={4}>
-              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-                xs=6 md=4
-              </Box>
-              </Grid>
-              <Grid xs={6} md={8}>
-              <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-                xs=6 md=8
-              </Box>
-              </Grid>
-            </Grid>
+            </Box>
         </div>
     )
 }
