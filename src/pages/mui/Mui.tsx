ec2fed296f6157935bb4e2890a38cae7c7c54606
Rick <cpchien@sinotech.org.tw>
2024-03-28 17:02:31 +0800

    container is the most basic layout element

--- src/pages/mui/Mui.tsx
+++ src/pages/mui/Mui.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';// Grid version 2
 import Toolbar from '@mui/material/Toolbar';
 
@@ -23,7 +24,7 @@ const Mui: React.FC = () => {
               <Toolbar />
               <h1 style={{ fontSize: '4em' }}>Try MUI Demo</h1>
             </Box>
-            <Box component="div" sx={{ flexGrow: 1, width : 0.85 }}>
+            <Container maxWidth="xl">
               <Grid container rowSpacing={5} columnSpacing={{ xs: 1, md: 5 }}>
                 <Grid xs={6} md={8}>
                   <Item>xs=6 md=8</Item>
@@ -38,7 +39,7 @@ const Mui: React.FC = () => {
                   <Item>xs=6 md=8</Item>
                 </Grid>
               </Grid>
-            </Box>
+            </Container>
         </div>
     )
 }
