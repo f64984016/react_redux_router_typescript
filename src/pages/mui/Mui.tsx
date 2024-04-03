bb139413469f81ecfffa720c4b0b0d6e3d4d664b
Rick <cpchien@sinotech.org.tw>
2024-04-03 16:49:48 +0800

    加入 button  元件測試

--- src/pages/mui/Mui.tsx
+++ src/pages/mui/Mui.tsx
@@ -8,6 +8,10 @@ import Toolbar from '@mui/material/Toolbar';
 
 import DrawerAppBar from '../../features/drawerAppBar/drawerAppBar';
 
+import { Button, IconButton  } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -27,7 +31,14 @@ const Mui: React.FC = () => {
             <Container maxWidth="xl">
               <Grid container rowSpacing={5} columnSpacing={{ xs: 1, md: 5 }}>
                 <Grid xs={6} md={8}>
-                  <Item>xs=6 md=8</Item>
+                  <Item>
+                    <Button sx={{margin:2}} variant='outlined' color='primary' onClick={() => {alert('clicked btn1');}}>btn1</Button>
+                    <Button sx={{margin:3}} variant='contained' color='secondary' size='small' startIcon={<DeleteIcon />}>btn2</Button>
+                    <Button sx={{margin:4}} variant='contained' color='warning' endIcon={<SendIcon />}>btn3</Button>
+                    <IconButton sx={{margin:3}} aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </Item>
                 </Grid>
                 <Grid xs={6} md={4}>
                   <Item>xs=6 md=4</Item>
